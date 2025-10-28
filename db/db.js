8263d04643b4517c73fdcbbc75fd12b5af791f01
calebdaradal <jeramydaradal@gmail.com>
2025-10-28 22:27:47 +0800

    bs

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 export default {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'tracerstudy'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 }
